ea53b95f6fc565eded90922f5dab4f47a8872319
jfoster39 <jfoster39@gatech.edu>
2016-04-01 21:21:21 -0400

    last

--- main.c
+++ main.c
@@ -1,34 +1,110 @@
 /**
 Name: Jonathan Foster
-2110 Homework 8
+2110 Homework 9
 **/
 
-#define REG_DISPCTL *(unsigned short *)0x4000000
-#define RGB(r, g, b) ((r) | ((g)<<5) | ((b)<<10))
-#define MODE3 3
-#define BG2_ENABLE (1<<10)
-typedef unsigned short u16;
-extern u16 *videoBuffer;
+#include <stdlib.h>
+#include <unistd.h>
+#include "mylib.h"
+#include "title.h"
+#include "ship.h"
+#include "enemy.h"
+#include "background.h"
+#include "entity.c" 
 
-void setPixel(int r, int c, u16 color);
-void drawRect(int r, int c, int width, int height, u16 color);
-void drawHollowRect(int r, int c, int width, int height, u16 color);
-void recursiveFun(int r, int c, int height, int width, u16 color);
+// Set up the Display control register to mode3, and BG2 enabled
+int shipRow;
+int shipCol;
+int enemyCol;
+int lives;
 
-int main(void) {
-	// Set up the Display control register to mode3, and BG2 enabled
-    REG_DISPCTL = MODE3 | BG2_ENABLE;
+typedef enum State{ TITLE, GAME_LOOP, GAME_OVER } State;
+extern State state;
 
-    // Infinitely draws the "fractal". Draws two sets of hollow 
-    // rectangle shapes over top of each other repeatedly. 
-    int i;
-    while(1) {
-    	for (i=0; i <= 256; i++) {
-   			recursiveFun(i, i, 170, 80, RGB(i/4, 0, 31+i));
-		}
-		for (i=0; i <= 256; i++) {
-   			recursiveFun(i, i, 170, 80, RGB(0, i/4, 31+i));
-		}
+State state = TITLE; 
+
+void titleScreen()
+{
+    // Set our game data
+    shipRow  = 55;
+    shipCol  = 0;
+    enemyCol = 0;
+    lives    = 3;
+
+    REG_DISPCNT = MODE3 | BG2_ENABLE;
+    DMA[3].src = title;
+    DMA[3].dst = videoBuffer;
+    DMA[3].cnt = (240*160) | DMA_ON;
+    while(!KEY_DOWN_NOW(BUTTON_START));
+
+    // This transitions to our game screen
+    wipeScreen( RGB( 0, 0, 0 ) );
+    state = GAME_LOOP;
+}
+
+void gameLoop()
+{
+    REG_DISPCNT = MODE3 | BG2_ENABLE;
+
+    EntityClass player_ship;
+    initialize_entity( &player_ship, SHIP_WIDTH, SHIP_HEIGHT, shipRow, shipCol );
+
+    while (lives != 0) 
+    {
+        // Draw background
+        drawImage3( 0, 0, BACKGROUND_WIDTH, BACKGROUND_HEIGHT, background );
+
+        // Draw ship
+        draw_entity( &player_ship, ship ); 
+
+        // Draw Enemy
+        drawImage3( 80, enemyCol, ENEMY_WIDTH, ENEMY_HEIGHT, enemy);
+        enemyCol--;
+
+        // Draw lives
+        //drawString3( 0, 0, "Lives: ", GREEN );
+
+        //if( lives == 3 )
+        //{
+        //    for( int i = 0; i < lives; i++ ) drawRect( 5, 220 + ( i + 10 ), 5, 5, GREEN );
+        //    for( int i = 0; i < lives; i++ ) drawRect( 5, 230 + ( i + 10 ), 5, 5, GREEN );
+        //    for( int i = 0; i < lives; i++ ) drawRect( 5, 240 + ( i + 10 ), 5, 5, GREEN );
+        //}
+
+        waitForVblank();
+
+        // If player hits 'select' return to titleScreen and start game over.
+        if( KEY_DOWN_NOW( BUTTON_SELECT ) )
+        {
+            state = TITLE;
+            break;
+        }
+
+        // Below are the checks for the player's ship's movement. 
+        if (KEY_DOWN_NOW(BUTTON_DOWN)) 
+        {
+            if(shipRow < 141)
+                move_entity( &player_ship, 4, 0 );
+        }
+        if (KEY_DOWN_NOW(BUTTON_UP)) 
+        {
+            if (shipRow > 20)
+                move_entity( &player_ship, -4, 0 );
+        }
     }
+}
+
+int main( void ) 
+{
+    while( 1 ) 
+    { 
+        if( state == TITLE )
+            titleScreen();
+        if( state == GAME_LOOP )
+           gameLoop();
+        if( state == GAME_OVER )
+            break;
+    }
+
     return 0;
 }
